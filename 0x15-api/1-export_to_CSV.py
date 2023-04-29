ba50adafb6286fbbaa691ca72aca05dc3751014a
KingHawkins <Kinghawkins61@gmail.com>
2023-04-29 23:20:17 +0300

    pudating

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,18 +8,17 @@ from sys import argv
 
 
 if __name__ == '__main__':
+    """Stores data in a csv file"""
     format_user = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
     format_todo = "https://jsonplaceholder.typicode.com/todos/"
     response_user = requests.get(format_user).json()
     response_todo = requests.get(format_todo).json()
     (name, usrname) = response_user.get('name'), response_user.get('username')
-    (all_tasks, completed, storage, status) = 0, 0, [], {}
+    (storage, status) = [], {}
     for item in response_todo:
         if item.get('userId') == int(argv[1]):
-            all_tasks += 1
             status[item.get('title')] = item.get('completed')
             if item.get('completed'):
-                completed += 1
                 storage.append("\t " + item.get('title'))
     with open(f"{argv[1]}.csv", "w", newline="") as cs:
         writer = csv.writer(cs)
